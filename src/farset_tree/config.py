8ed3e94f137ea6e77e0d6de5793675bc34de181c
Alexander Wilde <alexander.wilde87@gmail.com>
2023-12-06 17:41:57 +0000

    WIP

---
+++ src/farset_tree/config.py
@@ -0,0 +1,10 @@
+AP_NAME = 'farset-tree'
+AP_PASSWORD = None
+AP_DOMAIN = 'http://farset-tree.local'
+
+WIFI_FILE = '/farset_tree/wifi.json'
+WIFI_MAX_ATTEMPTS = 3
+
+GITHUB_FILE = '/farset_tree/github.json'
+
+LOG_FILE = '/farset_tree/log.txt'
